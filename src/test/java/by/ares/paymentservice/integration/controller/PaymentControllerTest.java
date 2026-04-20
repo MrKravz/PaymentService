0ba4494137276efac5b078580029490d9693a607
nparhomchik <parhomnik443@gmail.com>
2026-04-20 20:54:35 +0300

    bugfix: fixed issue that failed integration test

--- src/test/java/by/ares/paymentservice/integration/controller/PaymentControllerTest.java
+++ src/test/java/by/ares/paymentservice/integration/controller/PaymentControllerTest.java
@@ -3,20 +3,25 @@ package by.ares.paymentservice.integration.controller;
 import by.ares.paymentservice.dto.request.PaymentRequest;
 import by.ares.paymentservice.model.Payment;
 import by.ares.paymentservice.repository.PaymentRepository;
+import by.ares.paymentservice.service.SecurityValidationService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import static by.ares.paymentservice.util.TestConstants.*;
 import static by.ares.paymentservice.util.TestModelBuilder.buildPayment;
 import static by.ares.paymentservice.util.TestModelBuilder.buildPaymentRequest;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 class PaymentControllerTest extends AbstractIntegrationTest {
 
+    @MockitoBean
+    private SecurityValidationService securityValidationService;
     @Autowired
     public PaymentRepository paymentRepository;
 
@@ -46,7 +51,9 @@ class PaymentControllerTest extends AbstractIntegrationTest {
         saveTestItem();
         saveTestItem();
         mockMvc.perform(get("/payments/search/by-user")
-                        .param("userId", USER_ID.toString()))
+                        .param("userId", USER_ID.toString())
+                        .header("X-User-Id", 1L)
+                        .header("X-User-Role", "ADMIN"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.content.length()").value(3));
     }
@@ -67,7 +74,9 @@ class PaymentControllerTest extends AbstractIntegrationTest {
         stubFindUserById();
         mockMvc.perform(post("/payments")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(request)))
+                        .content(objectMapper.writeValueAsString(request))
+                        .header("X-User-Id", 1L)
+                        .header("X-User-Role", "ADMIN"))
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.id").exists())
                 .andExpect(jsonPath("$.orderId").value(ORDER_ID))
@@ -77,7 +86,7 @@ class PaymentControllerTest extends AbstractIntegrationTest {
 
     @Test
     void findTotalPaymentsSum_shouldReturnTotal() throws Exception {
-        mockMvc.perform(get("/payments/statistics/total")
+        mockMvc.perform(get("/payments/statistics")
                         .contentType(MediaType.APPLICATION_JSON)
                         .param("start", START_DATE.toString())
                         .param("end", END_DATE.toString()))
@@ -86,10 +95,12 @@ class PaymentControllerTest extends AbstractIntegrationTest {
 
     @Test
     void findTotalPaymentsSumByUserId_shouldReturnTotal() throws Exception {
-        mockMvc.perform(get("/payments/statistics/total/{userId}", USER_ID)
+        mockMvc.perform(get("/payments/statistics/users/{userId}", USER_ID)
                         .contentType(MediaType.APPLICATION_JSON)
                         .param("start", START_DATE.toString())
-                        .param("end", END_DATE.toString()))
+                        .param("end", END_DATE.toString())
+                        .header("X-User-Id", 1L)
+                        .header("X-User-Role", "ADMIN"))
                 .andExpect(status().isOk());
     }
 
--- src/test/java/by/ares/paymentservice/util/TestConstants.java
+++ src/test/java/by/ares/paymentservice/util/TestConstants.java
@@ -6,13 +6,16 @@ import java.time.LocalDate;
 
 public class TestConstants {
     public static final Long USER_ID = 1L;
+    public static final Long USER_ID_2 = 2L;
     public static final Long ORDER_ID = 2L;
     public static final Long PAYMENT_AMOUNT = 200L;
     public static final Long TOTAL_RESULT = 10000L;
     public static final Status PAYMENT_STATUS = Status.SUCCESS;
     public static final String TOPIC_NAME = "test_topic";
     public static final String EVENT = "event";
-    public static final LocalDate START_DATE = LocalDate.of(2026, 01, 01);
-    public static final LocalDate END_DATE = LocalDate.of(2026, 01, 01);
+    public static final LocalDate START_DATE = LocalDate.of(2026, 1, 1);
+    public static final LocalDate END_DATE = LocalDate.of(2026, 1, 1);
+    public static final String ADMIN = "ADMIN";
+    public static final String USER = "USER";
 
 }
